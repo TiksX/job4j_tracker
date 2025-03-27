958329dba4c12b8960eafe68ef6deb39f085f627
TiksX <tikssanek@gmai.com>
2025-03-27 20:36:34 +0300

    3.0. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,31 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
 
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Выбрать: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+
+    }
+
     public static void main(String[] args) {
-        Item item = new Item("Test Item", 1);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-EEEE-yyyy HH:mm:ss");
-        String formattedDate = item.getCreated().format(formatter);
-        System.out.println(formattedDate);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
 
